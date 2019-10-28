7789bd8332d4525ac128bcb32d5065fde2c727e7
Johannes Svensson <johannes.svensson97@gmail.com>
2019-10-28 11:10:09 +0100

    First commit and push to git, master branch

--- src/main/java/packet/Controller.java
+++ src/main/java/packet/Controller.java
@@ -5,18 +5,19 @@ import java.util.EventListener;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
+    public Label blank;
+
     public void clickToRestart(){
         System.out.println("funka");
     }
 
-    public void changePlace(){
-
-    }
-
-    public void dragOver(ActionEvent e){
-        System.out.println("hej");
+    public void changePlace(ActionEvent e){
+        Button temp = (Button) e.getSource();
+        System.out.println("funka");
     }
 }
