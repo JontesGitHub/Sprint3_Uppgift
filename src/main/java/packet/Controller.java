926009bc886b949411713361fecdcf2857c867b4
Johannes Svensson <johannes.svensson97@gmail.com>
2019-10-31 10:58:18 +0100

    switched to private variables in Controller class.

--- src/main/java/packet/Controller.java
+++ src/main/java/packet/Controller.java
@@ -12,10 +12,12 @@ import javafx.scene.layout.GridPane;
 
 public class Controller {
     public Label blank;
-    public int blankColumn = 3;
-    public int blankRow = 3;
+    private int blankColumn = 3;
+    private int blankRow = 3;
+
     public Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15;
     public GridPane gridPane;
+
     private List<Button> lista = new ArrayList<>();
     private boolean isAddedToList = false;
 
